7f32ba72978c36d6665d8857911b19b94df4b341
SAIKAT SAMANTA <saikat.samanta@hy-vee.com>
2024-11-25 00:34:04 +0530

    themecontroller

--- components/custom/SwipeableTasks.jsx
+++ components/custom/SwipeableTasks.jsx
@@ -16,18 +16,20 @@ import DraggableFlatList, {
 
 } from 'react-native-draggable-flatlist';
 import { NAV_THEME } from '~/lib/constants';
-import { colorScheme } from 'nativewind';
+import { useColorScheme } from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
 const OVERSWIPE_DIST = 20;
 const NUM_ITEMS = 20;
 
 
 function SimpleSwipable(props) {
-const themecontrol = NAV_THEME[colorScheme === "light" ? "light" : "dark"]
   const [data, setData] = useState(props.tasks);
   const itemRefs = useRef(new Map());
-
-  const renderItem = useCallback((params) => {
+  const colorScheme = useColorScheme();
+  console.log('colorScheme', colorScheme)
+  const themeText=NAV_THEME[colorScheme === "light" ? "light" : "dark"].text;
+  console.log('TextColor', themeText )
+  const renderItem = (params) => {
     const onPressDelete = () => {
       LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
       setData((prev) => {
@@ -38,8 +40,88 @@ const themecontrol = NAV_THEME[colorScheme === "light" ? "light" : "dark"]
     return (
       <RowItem {...params} itemRefs={itemRefs} onPressDelete={onPressDelete} />
     );
-  }, []);
+  };
 
+  function RowItem({ item, itemRefs, drag, onPressDelete }) { 
+    return (
+      <View style={styles.itemBreak}>
+        <SwipeableItem
+          key={item.key}
+          item={item}
+          ref={(ref) => {
+            if (ref && !itemRefs.current.get(item.key)) {
+              itemRefs.current.set(item.key, ref);
+            }
+          }}
+          onChange={({ openDirection }) => {
+            if (openDirection !== OpenDirection.NONE) {
+              [...itemRefs.current.entries()].forEach(([key, ref]) => {
+                if (key !== item.key && ref) ref.close();
+              });
+            }
+          }}
+          overSwipe={OVERSWIPE_DIST}
+          renderUnderlayLeft={() => (
+            <UnderlayLeft drag={drag} onPressDelete={onPressDelete} />
+          )}
+          renderUnderlayRight={() => <UnderlayRight />}
+          snapPointsLeft={[150]}
+          snapPointsRight={[150]}>
+  
+          <TouchableOpacity
+  
+            activeOpacity={1}
+            onLongPress={drag}
+            style={styles.sliders}
+            className='border-2 rounded-full'>
+  
+            <View className='flex-row items-center h-full px-5'>
+              <Image source={{ uri: item.image }} style={{ height: 20, width: 20, marginRight: 20 }} />
+              <View>
+                <Text className='text-2xl text-foreground'> {item.name} </Text>
+              </View>
+            </View>
+  
+          </TouchableOpacity>
+        </SwipeableItem>
+      </View>
+    );
+  }
+
+  const UnderlayLeft = ({
+    drag,
+    onPressDelete,
+  }) => {
+    const { item, percentOpen } = useSwipeableItemParams();
+    const animStyle = useAnimatedStyle(
+      () => ({
+        opacity: percentOpen.value,
+      }),
+      [percentOpen]
+    );
+  
+    return (
+      <Animated.View
+        style={[styles.row, styles.underlayLeft, animStyle]} // Fade in on open
+      >
+        <TouchableOpacity onPress={onPressDelete} >
+          <Feather name="trash-2" size={24} color={themeText} />
+        </TouchableOpacity>
+      </Animated.View>
+    );
+  };
+  
+  function UnderlayRight() {
+    const { close } = useSwipeableItemParams();
+    return (
+      <Animated.View style={[styles.row, styles.underlayRight]}>
+        <TouchableOpacity onPressOut={() => close()}>
+          <Text style={styles.text}>CLOSE</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    );
+  }
+  
   return (
     <View style={styles.container}>
       <DraggableFlatList
@@ -48,7 +130,6 @@ const themecontrol = NAV_THEME[colorScheme === "light" ? "light" : "dark"]
         renderItem={renderItem}
         onDragEnd={({ data }) => setData(data)}
         activationDistance={20}
-
       />
     </View>
   );
@@ -56,91 +137,9 @@ const themecontrol = NAV_THEME[colorScheme === "light" ? "light" : "dark"]
 
 export default SimpleSwipable;
 
-function RowItem({ item, itemRefs, drag, onPressDelete }) {
-const themecontrol = NAV_THEME[colorScheme === "light" ? "light" : "dark"]
-  return (
-    <View style={styles.itemBreak}>
-      <SwipeableItem
-        key={item.key}
-        item={item}
-        ref={(ref) => {
-          if (ref && !itemRefs.current.get(item.key)) {
-            itemRefs.current.set(item.key, ref);
-          }
-        }}
-        onChange={({ openDirection }) => {
-          if (openDirection !== OpenDirection.NONE) {
-            [...itemRefs.current.entries()].forEach(([key, ref]) => {
-              if (key !== item.key && ref) ref.close();
-            });
-          }
-        }}
-        overSwipe={OVERSWIPE_DIST}
-        renderUnderlayLeft={() => (
-          <UnderlayLeft drag={drag} onPressDelete={onPressDelete} />
-        )}
-        renderUnderlayRight={() => <UnderlayRight />}
-        snapPointsLeft={[150]}
-        snapPointsRight={[150]}>
-
-        <TouchableOpacity
-
-          activeOpacity={1}
-          onLongPress={drag}
-          style={styles.sliders}
-          className='border-2 rounded-full'>
-
-          <View className='flex-row items-center h-full px-5'>
-            <Image source={{ uri: item.image }} style={{ height: 20, width: 20, marginRight: 20 }} />
-            <View>
-              <Text className='text-2xl text-foreground'> {item.name} </Text>
-            </View>
-          </View>
-
-        </TouchableOpacity>
-      </SwipeableItem>
-    </View>
-  );
-}
-
-const UnderlayLeft = ({
-  drag,
-  onPressDelete,
-}) => {
-  const { item, percentOpen } = useSwipeableItemParams();
-  const animStyle = useAnimatedStyle(
-    () => ({
-      opacity: percentOpen.value,
-    }),
-    [percentOpen]
-  );
-
-  return (
-    <Animated.View
-      style={[styles.row, styles.underlayLeft, animStyle]} // Fade in on open
-    >
-      <TouchableOpacity onPress={onPressDelete} >
-        <Feather name="trash-2" size={24} color={themecontrol.text} />
-      </TouchableOpacity>
-    </Animated.View>
-  );
-};
-
-function UnderlayRight() {
-  const { close } = useSwipeableItemParams();
-  return (
-    <Animated.View style={[styles.row, styles.underlayRight]}>
-      <TouchableOpacity onPressOut={() => close()}>
-        <Text style={styles.text}>CLOSE</Text>
-      </TouchableOpacity>
-    </Animated.View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
   },
   row: {
 
@@ -148,8 +147,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-   
-    
   },
   text: {
     fontWeight: 'bold',
@@ -174,9 +171,6 @@ const styles = StyleSheet.create({
     height: 60,
     flex: 'row',
     alignItems: 'left',
-
-    backgroundColor: themecontrol.border,
-    borderColor: themecontrol.border
   },
   itemBreak:{
     marginBottom:10
